3e2ae08215c094ba250d535ea6cff6a657973c11
mykhailosondei <mykhailosondei@gmail.com>
2023-09-10 18:25:31 -0500

    validate RegisterUserDto props

--- ApplicationCommon/DTOs/User/RegisterUserDTO.cs
+++ ApplicationCommon/DTOs/User/RegisterUserDTO.cs
@@ -9,9 +9,9 @@ namespace ApplicationCommon.DTOs.User
 {
     public class RegisterUserDTO
     {
-        public int UsertId { get; set; }
+        public int UserId { get; set; }
         public string? Avatar { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public string Password { get; set; }
