4cc5a2f930a099de2b9232dbb0f86d574b47a16b
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2023-10-22 17:43:22 -0500

    added outside-closer to edit dialog

--- group-project-thread/ClientApp/src/app/Components/user-update-dialog/user-update-dialog.component.ts
+++ group-project-thread/ClientApp/src/app/Components/user-update-dialog/user-update-dialog.component.ts
@@ -1,16 +1,26 @@
-import {Component, Inject, NgModule, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, NgModule, OnInit, } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {UpdateUserDialogData} from "../../models/user/updateUserDialogData";
 import {faCamera, faXmark} from "@fortawesome/free-solid-svg-icons";
 import {Image} from "../../models/image";
-import DevExpress from "devextreme/bundles/dx.all";
-import data = DevExpress.data;
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 
 @Component({
   selector: 'app-user-update-dialog',
   templateUrl: './user-update-dialog.component.html',
-  styleUrls: ['./user-update-dialog.component.scss']
+  styleUrls: ['./user-update-dialog.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0,
+      })),
+      state('*', style({
+        opacity: 1,
+      })),
+      transition('void <=> *', animate('300ms ease-in-out')),
+    ]),
+  ]
 })
 export class UserUpdateDialogComponent implements OnInit{
   faCross = faXmark;
@@ -23,11 +33,15 @@ export class UserUpdateDialogComponent implements OnInit{
   maxCharCountLocation: number = 30;
   charCountBio: number = this.data.bio.length;
   maxCharCountBio: number = 100;
+  showContent : boolean;
+
 
   constructor(public dialogRef: MatDialogRef<UserUpdateDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: UpdateUserDialogData) {
   }
-  ngOnInit(): void {
+  ngOnInit() {
+    setTimeout(()=>this.showContent=true, 200);
   }
+
   onNoClick(): void {
     this.dialogRef.close();
   }
