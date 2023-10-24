422e5940f752d0df71e262df4359c4785f384783
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2023-10-24 01:26:51 -0500

    singular post and user hover refactored

--- group-project-thread/ClientApp/src/app/Components/singular-comment-view/singular-comment-view.component.ts
+++ group-project-thread/ClientApp/src/app/Components/singular-comment-view/singular-comment-view.component.ts
@@ -131,10 +131,7 @@ export class SingularCommentViewComponent {
     } else if (comment.post) {
       this.parentPost = comment.post;
     }
-    console.log("aaaaaaaaaaaaaaa");
-    console.log(this.parentPost);
   }
-
 
   handleLikeClick() {
     switch (this.liked) {
@@ -149,18 +146,6 @@ export class SingularCommentViewComponent {
         this.commentService.likeComment(this.commentView.id).subscribe(response => console.log(response));
         break;
     }
-  }
-
-  getCircleColor() {
-    return PostFormatter.getCircleColor(this.authorInput.username);
-  }
-
-  isAvatarNull() {
-    return this.authorInput.avatar === null;
-  }
-
-  getFirstInitial() {
-    return this.authorInput.username[0].toUpperCase();
   }
 
   protected readonly faPen = faPen;
